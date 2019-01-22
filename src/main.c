15094d9ee398f934c6b58783dec1ddf67b3fe1b0
mol <maulana2904@gmail.com>
2019-01-23 01:55:14 +0700

    Add test to main.c

--- src/main.c
+++ src/main.c
@@ -3,5 +3,7 @@
 int main(struct multiboot *mboot_ptr){
     monitor_clear();
     monitor_write("Hello, world");
+    asm volatile("int $0x3");
+    asm volatile("int $0x4");
     return 0xDEADBABA;
 }
