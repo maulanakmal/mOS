65db4e156c1a601624477c796964bc1a01f0ed0b
mol <maulana2904@gmail.com>
2019-01-23 16:22:45 +0700

    Refactor gdt and idt header

--- src/idt.h
+++ src/idt.h
@@ -1,26 +1,7 @@
-#ifndef DESCRIPTOR_TABLES_H
+#ifndef IDT_H
+#define IDT_H
 
 #include "common.h"
-
-struct gdt_entry_struct {
-    u16int limit_low;
-    u16int base_low;
-    u8int base_middle;
-    u8int access;
-    u8int granularity;
-    u8int base_high;
-} __attribute__((packed));
-
-typedef struct gdt_entry_struct gdt_entry_t;
-
-struct gdt_ptr_struct {
-    u16int limit;
-    u32int base;
-} __attribute__((packed));
-
-typedef struct gdt_ptr_struct gdt_ptr_t;
-
-void init_descriptor_tables();
 
 struct idt_entry_struct {
     u16int base_low;
@@ -72,5 +53,4 @@ extern void isr29();
 extern void isr30();
 extern void isr31();
 
-#define DESCRIPTOR_TABLES_H
 #endif
