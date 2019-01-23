a75aa364156b9261c948d841346dcf8b81b7bcc4
mol <maulana2904@gmail.com>
2019-01-23 21:27:58 +0700

    Add new line to Hello World in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@ int main(struct multiboot *mboot_ptr){
     init_gdt();
     init_idt();
     monitor_clear();
-    monitor_write("Hello, world!");
+    monitor_write("Hello, world!\n");
     asm volatile("int $0x3");
     asm volatile("int $0x4");
     return 0xDEADBEAF;
