cca647b67c3d949c9433195a9314e868c7bef60e
mol <maulana2904@gmail.com>
2019-01-23 16:32:15 +0700

    Update main.c to include separate deriptor header

--- src/main.c
+++ src/main.c
@@ -1,9 +1,12 @@
 #include "monitor.h"
+#include "gdt.h"
+#include "idt.h"
 
 int main(struct multiboot *mboot_ptr){
+    init_descriptor_tables();
     monitor_clear();
-    monitor_write("Hello, world");
+    monitor_write("Hello, world!");
     asm volatile("int $0x3");
     asm volatile("int $0x4");
-    return 0xDEADBABA;
+    return 0xDEADBEAF;
 }
