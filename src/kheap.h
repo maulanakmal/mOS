05ec95e25bf032998af8f016300047df162a1be8
mol <maulana2904@gmail.com>
2019-01-25 17:02:34 +0700

    Add kheap.c

--- src/kheap.h
+++ src/kheap.h
@@ -3,6 +3,7 @@
 
 #include "common.h"
 
+u32int kmalloc_int(u32int sz, int align, u32int *phys);
 u32int kmalloc(u32int sz);
 u32int kmalloc_a(u32int sz);
 u32int kmalloc_p(u32int sz, u32int *phys);
