a5164a70e3a68356150d57d16dc9595511fd56b7
mol <maulana2904@gmail.com>
2019-01-25 16:46:53 +0700

    Add kheap.h

---
+++ src/kheap.h
@@ -0,0 +1,11 @@
+#ifndef KHEAP_H
+#define KHEAP_H
+
+#include "common.h"
+
+u32int kmalloc(u32int sz);
+u32int kmalloc_a(u32int sz);
+u32int kmalloc_p(u32int sz, u32int *phys);
+u32int kmalloc_ap(u32int sz, u32int *phys);
+
+#endif
