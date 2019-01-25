65ee5162faeec40a534f4a04d272cd11171592ac
mol <maulana2904@gmail.com>
2019-01-25 16:41:59 +0700

    Add paging.h

---
+++ src/paging.h
@@ -0,0 +1,33 @@
+#ifndef PAGING_H
+#define PAGING_H
+
+#include "common.h"
+#include "isr.h"
+
+typedef struct page {
+    u32int present      : 1;
+    u32int rw           : 1;
+    u32int user         : 1;
+    u32int accessed     : 1;
+    u32int dirty        : 1;
+    u32int unused       : 7;
+    u32int frame        : 20;
+} page_t;
+
+typedef struct page_table {
+    page_t pages[1024];
+} page_table_t;
+
+typedef struct page_directory {
+    page_table_t *tables[1024];
+    u32int tables_physical[1024];
+    u32int physical_addr;
+} page_directory_t;
+
+void init_paging();
+
+page_t *get_page(u32int address, int make, page_directory_t *dir);
+
+void page_fault(registers_t regs);
+
+#endif
