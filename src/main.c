4eed9946636220726f614ffbe670e4ee432970ed
mol <maulana2904@gmail.com>
2019-01-25 11:57:53 +0700

    Add init_timer

--- src/main.c
+++ src/main.c
@@ -1,13 +1,16 @@
 #include "monitor.h"
 #include "gdt.h"
 #include "idt.h"
+#include "timer.h"
 
 int main(struct multiboot *mboot_ptr){
+    monitor_clear();
     init_gdt();
     init_idt();
-    monitor_clear();
+    init_timer(50);
     monitor_write("Hello, world!\n");
     asm volatile("int $0x3");
     asm volatile("int $0x4");
+    asm volatile("sti");
     return 0xDEADBEAF;
 }
